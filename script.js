b06764c577654891f76f369b2b14dca40d08dccf
Lovesingh2007 <shreeshyam12356ji@gmail.com>
2026-07-14 01:38:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function searchProduct() {
+    let p = document.getElementById("product").value.trim();
 
+    if (p === "") {
+        alert("Please enter a product name");
+        return;
+    }
+
+    document.getElementById("result").style.display = "block";
+    document.getElementById("title").innerText = p;
+
+    document.getElementById("amazon").href =
+        "https://www.amazon.in/s?k=" + encodeURIComponent(p);
+
+    document.getElementById("flipkart").href =
+        "https://www.flipkart.com/search?q=" + encodeURIComponent(p);
+
+    document.getElementById("meesho").href =
+        "https://www.meesho.com/search?q=" + encodeURIComponent(p);
+}
